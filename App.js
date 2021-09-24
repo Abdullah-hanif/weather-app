1551cf7a1b678a849d15c7102462ab2783201af1
Abdullah-hanif <79513564+Abdullah-hanif@users.noreply.github.com>
2021-09-25 04:42:38 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,56 @@
-
+import React,{useState,useEffect} from "react";
+import Input from './components/Input/index';
+import "./components/css/style.css";
+ 
 function App() {
+
+  const  [city,setCity] = useState(null);
+  const[search,setSearch] = useState("karachi");
+  
+  
+  useEffect(() => {
+  const fetchApi = async () => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=5e897d31900760aefc440b3f0109aa75`
+
+
+  const response = await fetch(url);
+  
+  const resJson= await response.json();
+//  console.log(resJson)  ;
+setCity(resJson.main);
+}
+
+
+  fetchApi ();
+  },[search] )
   return (
-    <div className="App">
+    <div className="box" >
+      <div className="inputData">
+        <Input type="search"
+        value={search}
+          placeholder="enter your city"
+          onChange={(event) => {setSearch(event.target.value) }}
+        />
+      </div>
+
+    {!city ? (
+      <p> not data found</p>
+    ): (
+<div>
+<h2>
+       {search}
+      </h2>
+      <h1>
+       {city.temp}*cel
+      </h1>
+      <h3>
+        min  : {city.temp_min} *cel | max : {city.temp_max}*cel
+      </h3>
+      </div>
+    )}
+
       
+    
     </div>
   );
 }
